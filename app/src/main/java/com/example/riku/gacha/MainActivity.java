535d1511680995869daff4592fd9440d36babdda
Akito <gilgamesh0190@yahoo.co.jp>
2015-10-27 00:44:19 +0900

    add WebView

--- app/src/main/java/com/example/riku/gacha/MainActivity.java
+++ app/src/main/java/com/example/riku/gacha/MainActivity.java
@@ -4,7 +4,8 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+//import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,19 +15,41 @@ import android.widget.TextView;
 
 import java.util.HashMap;
 
-public class MainActivity extends ActionBarActivity {
+//webviwe
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+//keyevent
+import android.view.KeyEvent;
+
+//WebView時のbarを削除
+//public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //imgInit
         ImageView image = (ImageView)findViewById(R.id.imageView);
         Resources res = getResources();
         Bitmap bitmap = BitmapFactory.decodeResource(res, R.drawable.gacha);
         Bitmap resize_bitmap = Bitmap.createScaledBitmap(bitmap, 300, 300, false);
         image.setImageBitmap(resize_bitmap);
 
+        //webview
+        //レイアウトで指定したWebViewのIDを指定する。
+        WebView  myWebView = (WebView)findViewById(R.id.webView1);
+
+        //リンクをタップしたときに標準ブラウザを起動させない
+        myWebView.setWebViewClient(new WebViewClient());
+
+        //最初にgoogleのページを表示する。
+        myWebView.loadUrl("file:///android_asset/index.html");
+
+        //jacascriptを許可する
+        myWebView.getSettings().setJavaScriptEnabled(true);
     }
 
     public void gacha(View view){
@@ -43,6 +66,16 @@ public class MainActivity extends ActionBarActivity {
 
         TextView tv = (TextView)findViewById(R.id.result_item);
         tv.setText((String) result.get("str"));
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        WebView  myWebView = (WebView)findViewById(R.id.webView1);
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
